30b572da501b871a9cd4cbbf0e59ec78940a12a4
Lapusya <lapusya2ru@gmail.com>
2026-04-07 10:57:50 +0500

    Some new features to calculator.py and test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,12 @@
-from src.calculator import add, subtract
+from src.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(10, 4) == 6
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(10, 2) == 5
